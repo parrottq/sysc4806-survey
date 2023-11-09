239f9cea6d3a1a519059cbf12dc376408ceb4dfe
Nicholas Rose <47899789+DualOrb@users.noreply.github.com>
2023-11-08 19:03:04 -0500

    Preliminary js for sending objects

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,6 +1,23 @@
 
+// Generates a UUID in the format expected by the backend
+function generateUUID() {
+    return this.crypto.randomUUID();
+}
 
 // Goes through the elements of a poll everywhere create survey and submits them via post to the backend
 function createPoll() {
     alert("It works!");
+    let questions = [];
+    //Formats each question into a json format
+    $('.question-container').each(function(index, element) {
+        // let question = {
+        //     "id"    : generateUUID(),
+        //     "title" : element.find('.question-title'),
+        //
+        // }
+        questions.push(question);
+    });
+
+
 }
+
