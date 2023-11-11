3841dc8e83d9b320ebf5ad21b49d68ac6d6a794f
TaoLufula <lifulaj@gmail.com>
2023-11-11 16:26:02 -0500

    Added test for getter and setter methods in poll test

--- src/test/java/SYSC4806/survey/tests/PollTest.java
+++ src/test/java/SYSC4806/survey/tests/PollTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class PollTest {
@@ -41,5 +42,24 @@ public class PollTest {
             var question1 = questions.get(0);
             assertThat(question1.getTitle()).isEqualTo("What?");
         }
+    }
+
+    @Test
+    void testSettersAndGetters() {
+        Poll poll = new Poll();
+        String title = "My sample Poll";
+        List<Question> questions = new ArrayList<>();
+
+        questions.add(new Question("Question 1", Question.Type.Text, new ArrayList<>(1), new ArrayList<>(2)));
+        questions.add(new Question("Question 2", Question.Type.MultipleChoice, new ArrayList<>(3), new ArrayList<>(4)));
+
+        poll.setTitle(title);
+        poll.setQuestions(questions);
+        assertFalse(poll.isClosed());
+        poll.setClosed(true);
+
+        assertEquals(title, poll.getTitle());
+        assertEquals(questions, poll.getQuestions());
+        assertTrue(poll.isClosed());
     }
 }
