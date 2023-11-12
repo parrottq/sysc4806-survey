5cfef95cdad1325d92409f42356dc32faa9c417e
mande <colinmandeville@cmail.carleton.ca>
2023-11-12 12:50:39 -0500

    Move test data into a DataJPATest class

--- src/main/java/SYSC4806/survey/Main.java
+++ src/main/java/SYSC4806/survey/Main.java
@@ -14,10 +14,6 @@ public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    public boolean checkIfTrue(boolean bool) {
-        return bool;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
@@ -26,78 +22,6 @@ public class Main {
     public CommandLineRunner demo(PollRepository pollRepository) {
         return (args) -> {
             log.info("Boot");
-
-            /*ArrayList<Answer> al0 = new ArrayList<>();
-
-            Answer a1 = new Answer("Good");
-            Answer a2 = new Answer("Bad");
-            Answer a3 = new Answer("Good");
-            Answer a4 = new Answer("Bad");
-            Answer a5 = new Answer("Good");
-            Answer a6 = new Answer("Good");
-            Answer a7 = new Answer("Good");
-            Answer a8 = new Answer("Bad");
-            ArrayList<Answer> al1 = new ArrayList<>();
-            al1.add(a1);
-            al1.add(a2);
-
-            ArrayList<Answer> al2 = new ArrayList<>();
-            al2.add(a3);
-            al2.add(a4);
-
-            ArrayList<Answer> al3 = new ArrayList<>();
-            al3.add(a5);
-            al3.add(a6);
-            al3.add(a7);
-            al3.add(a8);
-
-            Question q1 = new Question("How are you?", Question.Type.Text, al0, al1);
-            Question q2 = new Question("What's up?", Question.Type.MultipleChoice, al2, al3);
-
-            ArrayList<Question> aq = new ArrayList<>();
-
-            aq.add(q1);
-            aq.add(q2);
-
-            Poll p = new Poll("Hey", aq);
-
-            pollRepository.save(p);
-
-            al0 = new ArrayList<>();
-
-            a1 = new Answer("Good");
-            a2 = new Answer("Bad");
-            a3 = new Answer("Good");
-            a4 = new Answer("Bad");
-            a5 = new Answer("Good");
-            a6 = new Answer("Good");
-            a7 = new Answer("Good");
-            a8 = new Answer("Bad");
-            al1 = new ArrayList<>();
-            al1.add(a1);
-            al1.add(a2);
-
-            al2 = new ArrayList<>();
-            al2.add(a3);
-            al2.add(a4);
-
-            al3 = new ArrayList<>();
-            al3.add(a5);
-            al3.add(a6);
-            al3.add(a7);
-            al3.add(a8);
-
-            q1 = new Question("How are you?", Question.Type.Text, al0, al1);
-            q2 = new Question("What's up?", Question.Type.MultipleChoice, al2, al3);
-
-            aq = new ArrayList<>();
-
-            aq.add(q1);
-            aq.add(q2);
-
-            p = new Poll("Yo", aq);
-
-            pollRepository.save(p); */
         };
     }
 }
