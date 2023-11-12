9c53617e501c058987c4d3555faa5ceb13119154
mande <colinmandeville@cmail.carleton.ca>
2023-11-12 12:52:03 -0500

    Change function arg to be more explanatory

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -147,8 +147,8 @@ function changeQuestionType(element) {
     }
 }
 
-function getResults(e) {
-    id = e.target.parentNode.querySelector('.poll_id').innerHTML.split(": ")[1];
+function getResults(clickEvent) {
+    id = clickEvent.target.parentNode.querySelector('.poll_id').innerHTML.split(": ")[1];
 
     // Redirect to page
     window.location.assign('/view-polls/results?id=' + id);
