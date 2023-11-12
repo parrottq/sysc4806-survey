0f263a6e7a26b20b7c60baba7e45ea96dd9b790e
mande <colinmandeville@cmail.carleton.ca>
2023-11-12 13:01:47 -0500

    Change test file name to reflect what is being tested

--- src/test/java/SYSC4806/survey/tests/PollRepositoryTest.java
+++ src/test/java/SYSC4806/survey/tests/PollRepositoryTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class MainTest {
+public class PollRepositoryTest {
 
     @Resource
     private PollRepository pollRepository;
