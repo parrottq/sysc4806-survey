497e7eddd7e4f753aee479e9b27092e54899ab01
ibrahimsaidhi <91332979+ibrahimsaidhi@users.noreply.github.com>
2023-11-26 18:36:30 -0500

    Implement Poll Closing
    * Implemented the closing of polls once they have been submitted
    
    * Re-implemented closing poll functionality so that a close button closes the poll rather than it closing automatically
    
    * minor changes that removed submitted field
    
    * more minor cleanup changes

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -58,18 +58,12 @@ function saveAnswers() {
         url: '/save-form',
         data: JSON.stringify(poll),
         contentType: "application/json; charset=utf-8",
-        success: function(text) {
-            alert("Poll has been submitted");
-        },
-        error: function() {
-            alert("Error");
-        }
     });
-
+    displayPolls();
 }
 
 /**
- * Redicrects the display page to the desired poll given the ID
+ * Redirects the display page to the desired poll given the ID
  * @param id
  */
 function redirectToPoll(id){
@@ -77,7 +71,20 @@ function redirectToPoll(id){
 }
 
 /**
- * Redicrects the display page to the desired poll given the ID
+ * closes the current poll, and alerts the backend to change status of poll
+ */
+function closePoll(){
+    let pollID = document.getElementById("view").getAttribute('data-name');
+    $.ajax({
+        type: 'POST',
+        url: '/close-poll',
+        data: JSON.stringify(pollID),
+        contentType: "application/json; charset=utf-8"
+    });
+}
+
+/**
+ * Redirects the display page to the desired poll given the ID
  * @param id
  */
 function redirectToPollResults(id){
